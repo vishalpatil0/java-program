2f1107425888dc582e8ff02549840af6582bc076
vishalpatil0 <vishalgpatil10@gmail.com>
2022-04-20 20:41:39 +0530

    Delete unnecessary files

--- Experiment1.java
+++ Experiment1.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 class Experiment1{
   public static void main(String[] args) {
-    int a=10;
-    System.out.println("Data type printing");
-    System.out.println(((Object)a).getClass().getSimpleName());
+    String str="Vishal";
+    String str1=new String("Vishal Patil");
+    System.out.println("String is "+str);
+    System.out.printf("String is %2.2s",str1);
+    System.out.println();
+    Scanner sc=new Scanner(System.in);
+    String str3=sc.nextLine();
+    System.out.format("String is %s",str3);
   }
 }
